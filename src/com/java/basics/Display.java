1e0f6df9ab2d10af4ba951cc39a81d1ae7a25d4a
VigneshNallasivam <vickyshanestark@gmail.com>
2022-12-23 20:27:14 +0530

    Added : MASTER - Displaying the Welcome Message for Mood Analyser Simulation Program

---
+++ src/com/java/basics/Display.java
@@ -0,0 +1,12 @@
+package com.java.basics;
+
+public class Display 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome To Mood Analyser Simulation Program");
+
+	}
+
+}
